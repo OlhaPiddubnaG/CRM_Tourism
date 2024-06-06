28f3852ab26b80357b3805a7149365575533f75a
olhapiddubna <babiy_olga474@ukr.net>
2024-06-06 17:56:36 +0300

    CRM-3-Create Login Directory and move files

--- CRM/CRM.Admin/Components/Pages/Login/Login.razor.cs
+++ CRM/CRM.Admin/Components/Pages/Login/Login.razor.cs
@@ -3,25 +3,29 @@ using CRM.Admin.Data;
 using CRM.Admin.Extensions;
 using CRM.Admin.Requests.AuthRequests;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
 using MudBlazor;
 
-namespace CRM.Admin.Components.Pages;
+namespace CRM.Admin.Components.Pages.Login;
 
 public partial class Login
 {
     [Inject] IAuthenticationRequest AuthenticationRequest { get; set; }
+    [Inject] AuthenticationStateProvider AuthenticationStateProvider  {get; set; }
     [Inject] CustomAuthenticationStateProvider CustomAuthStateProvider { get; set; }
     [Inject] NavigationManager NavigationManager { get; set; }
     [Inject] IJSRuntime JSRuntime { get; set; }
 
     private string _email;
     private string _password;
-    private bool _shouldSetAuthToken;
     private string _accessToken;
     private string _message;
     private Severity _alertSeverity;
+    private bool _shouldSetAuthToken;
+    private bool isAdmin = false;
 
+ 
     public async Task LogIn()
     {
         var response = await AuthenticationRequest.Login(new LoginModel { Email = _email, Password = _password });
@@ -30,26 +34,20 @@ public partial class Login
         {
             await JSRuntime.InvokeVoidAsync("localStorage.setItem", "authToken", response.AccessToken);
             CustomAuthStateProvider.NotifyUserAuthentication(response.AccessToken);
-            NavigationManager.NavigateTo("/home");
+            
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity.IsAuthenticated)
+        {
+            isAdmin = user.IsInRole("Admin");
+        }
+            NavigationManager.NavigateTo("/client");
         }
         else
         {
             _message = "Authentication failed";
             _alertSeverity = Severity.Error;
-        }
-    }
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (_shouldSetAuthToken)
-        {
-            _shouldSetAuthToken = false;
-            if (JSRuntime.IsInvokeable())
-            {
-                await JSRuntime.InvokeVoidAsync("localStorage.setItem", "authToken", _accessToken);
-                CustomAuthStateProvider.NotifyUserAuthentication(_accessToken);
-                NavigationManager.NavigateTo("/home");
-            }
         }
     }
 
